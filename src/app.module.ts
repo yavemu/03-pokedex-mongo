f196c33acc467725de615f09a3a201a6d79ce516
YAMid <yavemu@gmail.com>
2022-08-25 19:09:41 -0500

    connecting to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
-  imports: [PokemonModule],
+  imports: [
+    PokemonModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+  ],
 })
 export class AppModule {}
