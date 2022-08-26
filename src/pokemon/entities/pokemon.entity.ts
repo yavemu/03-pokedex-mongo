c40d88f7c38f6a9cff853b640a459fa2ffadbdf5
YAMid <yavemu@gmail.com>
2022-08-25 19:48:57 -0500

    Create Entity Schema and import

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,19 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Pokemon extends Document {
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  pokemon_id: number;
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
