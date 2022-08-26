5b1ebe26512034fb65fb51fce13a498fe0288256
YAMid <yavemu@gmail.com>
2022-08-25 20:06:26 -0500

    add dto validations

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,20 @@
-export class CreatePokemonDto {}
+import {
+  IsInt,
+  IsNotEmpty,
+  IsPositive,
+  IsString,
+  Min,
+  MinLength,
+} from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  pokemon_id: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  name: string;
+}
